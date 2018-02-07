5f1e50a15d41564f1bdb85fbdefa92b9df238771
Justin Robertson <jusrobertson@yahoo.co.uk>
2018-02-08 06:20:11 +0800

    Whole project

---
+++ src/contactmanager/ContactApp.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactmanager;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author justin
+ */
+public class ContactApp {
+
+    Contact[] contacts = new Contact[100];
+    int contactCount = 0;
+    DBHandler dbHandler = new DBHandler();
+
+    void addContact(
+            String firstName,
+            String lastName,
+            String phone,
+            String email) {
+        Contact contact = new Contact();
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setPhone(phone);
+        contact.setEmail(email);
+
+        contacts[contactCount++] = contact;
+        
+        dbHandler.insertRow(firstName, lastName, phone, email);
+    }
+
+    String[] getContactsList() {
+        String[] stringArray = new String[contactCount];
+        for (int i = 0; i < contactCount; i++) {
+            if (contacts[i] != null) {
+                stringArray[i] = contacts[i].toString();
+            }
+        }
+        return stringArray;
+    }
+
+    String[] getContactFields(int selected) {
+
+        if (selected > -1) {
+            Contact c = contacts[selected];
+            String[] fields = new String[4];
+            fields[0] = c.getFirstName();
+            fields[1] = c.getLastName();
+            fields[2] = c.getPhone();
+            fields[3] = c.getEmail();
+            return fields;
+        } else {
+            return null;
+        }
+    }
+
+    boolean deleteContact(int index) {
+        if (index > -1 && index < contactCount) {
+            for (int i = index; i < contactCount - 1; i++) {
+                contacts[i] = contacts[i + 1];
+
+            }
+            contactCount = contactCount - 1;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    boolean editContact(
+            int index, String firstName,
+            String lastName,
+            String phone,
+            String email) {
+        contacts[index].setFirstName(firstName);
+        contacts[index].setLastName(lastName);
+        contacts[index].setPhone(phone);
+        contacts[index].setEmail(email);
+        return true;
+    }
+
+}
---
+++ src/contactmanager/DBHandler.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactmanager;
+
+import java.sql.*;
+
+/**
+ *
+ * @author justin
+ */
+public class DBHandler {
+
+    Connection connection = null;
+
+    DBHandler() {
+        try {
+
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:contacts.db");
+            System.out.println("Connected to contacts database.");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());            
+        }
+        
+
+    }
+
+    boolean insertRow(
+            String firstName,
+            String lastName,
+            String phone,
+            String email) {
+        String sql = "insert into Contact (FirstName, LastName, Phone, Email) values (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, firstName);
+            ps.setString(2, lastName);
+            ps.setString(3, email);
+            ps.setString(4, phone);
+            ps.executeQuery();
+            ResultSet rs = ps.getGeneratedKeys();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return true;
+
+    }
+
+    String[] selectRow() {
+        String[] row = null;
+        String sql = "insert into Country (CountryName, Population) values (?, ?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+
+        }
+        return row;
+    }
+    
+    
+}
